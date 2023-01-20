1a0284904949e2d9db103569e679ec5adaab7a56
Hamwall0 <116351425+Hamwall0@users.noreply.github.com>
2023-01-20 13:19:33 +0100

    Add files via upload

--- äventrysr spel.py
+++ äventrysr spel.py
@@ -1,22 +1,25 @@
 import random as rand
 import time
+import threading
+# Välkomen :D
+# Här uppe ligger alla våra classer och deras egenskaper
 
-class Fiende():
-    def __init__(self, monster_namn):
+class Fiende(): 
+    def __init__(self, monster_namn, monster_styrka, monster_hp):
         self.namn = monster_namn
-        self.styrka = rand.randint(6,9)
-        self.hp = rand.randint(20,30)
+        self.styrka = monster_styrka
+        self.hp = monster_hp
             
 class Cockroach_type(Fiende):
-    def __init__(self, monster_namn,):
-        super().__init__(monster_namn,)
-        self.hp = rand.randint(21,40)
+    def __init__(self, monster_namn, monster_styrka, monster_hp):
+        super().__init__(monster_namn, monster_styrka, monster_hp)
+        
 
     
 class Sheep_type(Fiende):
-    def __init__(self, monster_namn, ):
-        super().__init__(monster_namn, )
-        self.hp = rand.randint(10,19)
+    def __init__(self, monster_namn, monster_styrka, monster_hp):
+        super().__init__(monster_namn, monster_styrka, monster_hp)
+    
 
 
 class item_attribut():
@@ -26,64 +29,70 @@ class item_attribut():
 
 class Spelare():
 
-    def __init__(self,namn, strength, hp, lvl, inventory, exp, exp_lim):
+    def __init__(self,namn, strength, hp, lvl, inventory, exp, exp_multi):
         self.strength = strength
         self.hp = hp
         self.lvl = lvl
         self.inventory = inventory
         self.namn = namn
         self.exp = exp
-        self.exp_lim = exp_lim
+        self.exp_multi = exp_multi
+
+    def intro(self):
+        spelare.namn = input("Namn ---> ")
+        time.sleep(0.25)
 
     def stats(self): # en funktion som skriver ut spelarens nuvarande stats
-        return print(f"""Name: {spelare.namn} 
-    STR: {spelare.strength} + {item_bonus()} 
-    HP: {spelare.hp}
-    LVL: {spelare.lvl}
-    EXP: {spelare.exp}/{40*spelare.exp_lim}""")
+        return print(f"""Namn: {spelare.namn}   STR: {spelare.strength} + {item_dmg()}    HP: {spelare.hp}
+    LVL: {spelare.lvl}  EXP: {spelare.exp}/{40*spelare.exp_multi}""")
     
     def backpack(self): # en funtkion som skriver ut spelarens föremål
         count = 1
-        for sak in self.inventory:
-            print(f"{count}.",sak.Type,"dmg:",sak.dmg,)
-            time.sleep(.25)
-            count +=1
+        for föremål in self.inventory: # Listar upp alla föremål i inventory
+            print(f"{count}.",föremål.Type,"DMG:",föremål.dmg,)
+            count += 1
         if len(self.inventory) > 5:
             manadge_items()
         elif len(self.inventory) > 0:
             while len(self.inventory)>0:
-                user_choice = input("\nvill du ta bort ett item y/n--> ")
-                if user_choice in ("Yes","Y","y","yes"):
+                user_choice = input("\nVill du ta bort ett item Ja/Nej--> ")
+                if user_choice.lower() in ("j","ja"):
                     manadge_items()
-                elif user_choice in ("No","no","N","n"):
+                elif user_choice.lower() in ("n","nej"):
                     break
                 else:
-                    print("du skrev något förbjudet")
+                    print("Du skrev in något förbjudet")
                     continue
         else:
-            print("\ndin rygsäck är tom")
+            print("\nDin ryggsäck är tom")
 
-    def lvl_up(self):
-        spelare.exp += 20
-        if spelare.exp == 40*spelare.exp_lim:
-            spelare.exp -= 40*spelare.exp_lim
+    def lvl_up(self): # Är en level up metod för spelaren.
+        spelare.exp += 40
+        if spelare.exp >= 40*spelare.exp_multi: # spelaren har en exp_multiplier som multipliceras med det första exp målet (40)
+            spelare.exp -= 40*spelare.exp_multi
             spelare.strength += 3
             spelare.lvl += 1
             spelare.hp += 5
-            spelare.exp_lim += 0.5
-            print(f"""du levlade upp! 
-[STR: {spelare.strength}]
-[LVL: {spelare.lvl}]
-[HP: {spelare.hp}]
-EXP: {spelare.exp}""")
+            spelare.exp_multi += 0.5
+            print(f"""\nDu levlade upp! 
+[Nuvarande STR: {spelare.strength}]   [Nuvarande LVL: {spelare.lvl}]    [Nuvarande HP: {spelare.hp}]""")
+
+class Scoreboard(Spelare):
+    def __init__(self, namn, strength, hp, lvl, inventory, exp, exp_multi):
+        super().__init__(namn, strength, hp, lvl, inventory, exp, exp_multi)
+    
+    def board(self):
+        print(f"""               SCOREBOARD
+                STRENGTH    HEALTH POINTS   LEVEL   TIME
+{spelare.namn}:         {spelare.strength}          {spelare.hp}            {spelare.lvl}  """)
       
 
-def manadge_items():# en funktion som hanterar spelaren föremål 
+def manadge_items():# en funktion som hanterar spelarens föremål 
     while True:
-        val = input("\nvälj  positionen på itemet du vill ta bort: ")
-        if val.isdigit() and int(val) <= len(spelare.inventory):
-            skräp = spelare.inventory.pop(int(val)-1)
-            print("You removed:", skräp.Type, "dmg:", skräp.dmg)
+        val_item = input("\nVälj positionen på itemet du vill ta bort: ")
+        if val_item.isdigit() and int(val_item) <= len(spelare.inventory):
+            skräp_item = spelare.inventory.pop(int(val_item)-1)
+            print("Du tog bort:", skräp_item.Type, "DMG:", skräp_item.dmg)
             break
         else:
             print("Inmätnings fel")
@@ -91,110 +100,146 @@ def manadge_items():# en funktion som hanterar spelaren föremål
                 
 def kista():
     while True:
-        items = ["penna", "sudd", "sword", "Dog"] # Är items som hittas i kista
+        items = ["Penna", "Sudd", "Sword", "Hund"] # Är items som hittas i kista
         item_chans = rand.randint(0,len(items)-1) # Definerar ett slumpmässigt item som "item_chans"
         item = item_attribut(rand.randint(1,5),items.pop(item_chans)) # Ger ett slumpmässigt föremål en slumpmässig styrka 
-        print("\ndu hittade en kista")
-        print("Du fick en:",item.Type,"DMG: ",item.dmg)
+        print("\nDu hittade en kista")
+        print(f"Du fick en [{item.Type}] DMG: {item.dmg}")
         spelare.lvl_up()
-        print("+ 10 EXP ")
+        print("Du fick 40 EXP ")
         spelare.inventory.append(item)
         if len(spelare.inventory) > 5:
-            print("\ndin rygsäck är full")
+            print("\nDin ryggsäck är full")
             spelare.backpack()
             break
         else:
-            if item_bonus() == 25:
-                print("\n Du har blivit DPS Gud! \n + 30 HP")
-                spelare.hp += 30
+            if item_dmg() >= 25:
+                random_dialogue = rand.randint(1,10)
+                if random_dialogue == 6:
+                    print("\nExploit the DPS God! \n + 30 HP")
+                    spelare.hp += 30
+                else:
+                    print("\n Du har blivit DPS Gud! \n + 30 HP")
+                    spelare.hp += 30
             return 
            
 
-def item_bonus():
+def item_dmg(): # räknar ut totala dmg bonusen spelare ska få 
     dmg_bonus = 0
-    for sak in spelare.inventory:
-        dmg_bonus += sak.dmg
+    for föremål in spelare.inventory:
+        dmg_bonus += föremål.dmg
     return(dmg_bonus)
 
-def combat():
-    list_of_fiende = [fiende, sheep, roaches]
-    fiende_chans = rand.randint(0,len(list_of_fiende)-1)
-    random_fiende = list_of_fiende.pop(fiende_chans)
-    monster_hp = rand.randint(5,10*spelare.lvl)
-    print(f"\n The battle begins! {spelare.namn} vs {random_fiende.namn}")
-    while spelare.hp > 0 or random_fiende.hp:
-        monster_hp == random_fiende.hp
-        input("press enter to attack ")
-        fiende.styrka = rand.randint(6,9)
-        sheep.styrka = rand.randint(3,5)
-        roaches.styrka = rand.randint(0,2)
-        random_fiende.hp -= spelare.strength + item_bonus()
-        spelare.hp -= random_fiende.styrka
-        print(f"\n {random_fiende.namn} dealt: {random_fiende.styrka}       {spelare.namn} dealt: {spelare.strength} ")
+def combat(): # Är combat systemet
+    full_dmg = item_dmg()
+    fiende.hp = rand.randint(20,30)
+    sheep.hp = rand.randint(10,20)
+    roaches.hp = rand.randint(20,40)
+    list_of_fiende = [fiende, sheep, roaches] #Lista av alla möjliga fiender
+    fiende_chans = rand.randint(0,len(list_of_fiende)-1) #Slumpar fram en av 3 möjliga fiender
+    random_fiende = list_of_fiende.pop(fiende_chans) #Väljer ut en av 3 möjliga fiender och definerar de som "random_fiende"
+    random_fiende.hp = rand.uniform(random_fiende.hp,random_fiende.hp*spelare.exp_multi)
+    print(f"\n Striden har börjat! {spelare.namn} vs {random_fiende.namn}")
+    while spelare.hp > 0 or random_fiende.hp: # random_fiende sätts in i en loop för att sloss för sitt liv
+        if input("\nTryck enter för att sloss ") == "":
+            pass
+        else:
+            print("Som sagt, tryck enter för att sloss")
+            continue
+        time.sleep(0.5)
+        fiende.styrka = rand.randint(9,11)
+        sheep.styrka = rand.randint(5,10)
+        roaches.styrka = rand.randint(0,9)
+        spelare.strength = round(rand.uniform(10, spelare.strength + full_dmg ))
+
+        random_fiende.hp -= spelare.strength
+        if random_fiende.hp <= 0:
+            random_fiende.styrka = 0
+            random_fiende.hp = 0
+        else:
+            spelare.hp -= random_fiende.styrka
+        print(f"\n{random_fiende.namn} dealt: {random_fiende.styrka}        {spelare.namn} dealt: {spelare.strength} ")
     
-        print(f"""{random_fiende.namn}'s HP = {random_fiende.hp}       {spelare.namn}'s HP = {spelare.hp} """)
+        print(f"""{random_fiende.namn}'s HP = {round(random_fiende.hp)}       {spelare.namn}'s HP = {spelare.hp} """)
         if spelare.hp <= 0:
-            print("\nYou lost the battle!")
+            print("\nDu förlora striden!")
             break
         elif random_fiende.hp <= 0:
-            print("\nThe monster was defeated!")
+            print("\nMonstret besegrades!")
             spelare.lvl_up()
-            print("you gained 10 EXP")
+            print("du fick 40 EXP")
             break
-#Ger chosen att alltingen fly eller attakera monstret.
+#Ger alternativ att alltingen fly eller attakera monstret.
 def monster():
     while True:
-        choice = input("run? y/n: ")
-        if choice in ("y","Y","Yes","YES"):
+        choice = input("Spring iväg? [Ja/Nej]: ")
+        if choice in ("j","ja","J","Ja"):
             chance = rand.randint(1,5)
             if chance in (1,2,3):
-                print("du lyckades fly från monstret")
+                print("Du lyckades fly från monstret")
                 break 
             else:
-                print("du lyckades inte fly")
+                print("Du lyckades inte fly")
                 combat()
                 break
-        elif choice in ("NO","N","n","No"):
+        elif choice in ("n","nej","N","Nej"):
             combat()
             break
         else:
-            print("du skrev något förbjudet")
+            print("Du skrev något förbjudet")
             continue
         
-            
+# Ignorence is bliss, ger illusionen av att man väljer 1 unik dörr när alla tre dörrar är igentligen samma dörr          
 def door():
+    print("Tryck 'M' för att komma till menyn")
     while spelare.hp > 0 and spelare.lvl < 10:
-        door = input("\nvälje en av tre dörrar ('1' '2' '3'): 4 = Meny --> ")
-        if door == "4":
+        door = input("""\nVälje en av tre dörrar (1-3)
+---> """)
+        if door in ("m", "M"):
             break
         elif door in("1","2","3"):
-            chans = rand.randint(1,5)
+            chans = rand.randint(1,3)
             if chans == 1:
-                print("\ndu trillade ned i en fälla")
+                print("\nDu trillade ned i en fälla")
                 print(f"HP:{spelare.hp} -1")
                 spelare.hp -= 1   
-            elif chans == 2 or chans == 3:
+            elif chans == 2:
                 kista()
-            elif chans == 4 or chans == 5:
-                print("\ndu stötte på ett monster")
+            elif chans == 3:
+                print("\nDu stötte på ett monster")
                 monster()
         else:
-            print("\ndet fins bara tre dörrar!")
+            print("\nDet fins bara tre dörrar!")
             continue
 
-roaches = Cockroach_type("Roach")
-sheep = Sheep_type("Sheep",)
-fiende = Fiende("Bertil",)
+def timer():
+    count = 0
+    while True:
+        time.sleep(1)
+        count += 1
+        if spelare.hp <= 0 or spelare.lvl == 10:
+            print("Time played: ", count//60 ," minutes", count, "seconds")
 
-spelare_namn = input("välje ett namn -> ") 
-spelare = Spelare(spelare_namn, 5, 20, 1,[], 0, 1)
+            break
 
-while spelare.hp>0 and spelare.lvl < 10:
-    choice = input(""" \nvad vill du göra
-1.öppna en dör      2.check status
 
-3.kolla ditt inventory 
-    ->""")
+# Object för classerna
+
+roaches = Cockroach_type("Roach", rand.randint(0,9), 1)
+sheep = Sheep_type("Sheep", rand.randint(5,12), 1)
+fiende = Fiende("Bertil", rand.randint(6,9), 1)
+spelare = Spelare("", 10, 100, 1,[], 0, 1)
+scoreboard = Scoreboard(spelare.namn, spelare.strength, spelare.hp, spelare.lvl, spelare.inventory, spelare.exp, spelare.exp_multi)
+spelare.intro()
+timer_thread = threading.Thread(target=timer, daemon=True)
+timer_thread.start()
+
+
+# Huvud program
+while spelare.hp > 0 and spelare.lvl < 10:
+    choice = input(""" \nVad vill du göra
+1.Öppna en dörr     2.Status    3.Inventory 
+    ---> """)
     if choice in ("1","2","3","4"):
         if choice == "1":
             door()
@@ -203,9 +248,10 @@ while spelare.hp>0 and spelare.lvl < 10:
         elif choice == "3":
             spelare.backpack()
     else:
-        print("du skrev in något förbjudet")
+        print("Du skrev in något förbjudet")
         continue
 if spelare.lvl == 10:
     print("DU VANN!")
 else:
-    print("GAME OVER")
+    print("GAME OVER :(")
+scoreboard.board() #Work in progress
